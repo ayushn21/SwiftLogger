6506f60ef8e3f2a50c561a94f7673a639717cdb8
Ayush Newatia <Ayush.Newatia@akqa.com>
2016-07-15 11:32:56 +0100

    moved some tests to a new messagetests class

--- SwiftLoggerTests/LoggingServiceTests.swift
+++ SwiftLoggerTests/LoggingServiceTests.swift
@@ -30,49 +30,4 @@ class LoggingServiceTests: XCTestCase {
         XCTAssertTrue(self.loggingService.dateFormatter.dateFormat == "dd-MM-yyyy HH:mm:ss.SSS",
             "The default date format should be correctly set")
     }
-    
-    func testMessageMetadataInitialiser() {
-        let file: NSString = #file
-        let function = #function
-        let line = #line as UInt
-        
-        let metadata = MessageMetadata(level: "Debug", file: file as String, function: function, line: line)
-        
-        XCTAssertTrue(metadata.level == "Debug", "The log level string should be correctly set")
-        XCTAssertTrue(metadata.file == file.lastPathComponent, "The file string should be correctly set")
-        XCTAssertTrue(metadata.function == function, "The function string should be correctly set")
-        XCTAssertTrue(metadata.line == line, "The line string should be correctly set")
-        XCTAssertNotNil(metadata.timestamp, "The date should have been created with the object")
-    }
-    
-    func testMessageInitialiser() {
-        let file: NSString = #file
-        let function = #function
-        let line = #line as UInt
-        
-        let message = Message("test", level: "Debug", file: file as String, function: function, line: line)
-        
-        XCTAssertTrue(message.body == "test", "The log message should be correctly set")
-        XCTAssertTrue(message.metadata.level == "Debug", "The log level string should be correctly set")
-        XCTAssertTrue(message.metadata.file == file.lastPathComponent, "The file string should be correctly set")
-        XCTAssertTrue(message.metadata.function == function, "The function string should be correctly set")
-        XCTAssertTrue(message.metadata.line == line, "The line string should be correctly set")
-        XCTAssertNotNil(message.metadata.timestamp, "The date should have been created with the object")
-    }
-    
-    func testMessageInitialiserWithMetdata() {
-        let file: NSString = #file
-        let function = #function
-        let line = #line as UInt
-        
-        let metadata = MessageMetadata(level: "Debug", file: file as String, function: function, line: line)
-        let message = Message("test", metadata: metadata)
-        
-        XCTAssertTrue(message.body == "test", "The log message should be correctly set")
-        XCTAssertTrue(message.metadata.level == "Debug", "The log level string should be correctly set")
-        XCTAssertTrue(message.metadata.file == file.lastPathComponent, "The file string should be correctly set")
-        XCTAssertTrue(message.metadata.function == function, "The function string should be correctly set")
-        XCTAssertTrue(message.metadata.line == line, "The line string should be correctly set")
-        XCTAssertNotNil(message.metadata.timestamp, "The date should have been created with the object")
-    }
 }
